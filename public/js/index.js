9c22b1734892e714a79c5e8ae56b027ef29271ac
Vincent van Leeuwen <shoockz@hotmail.com>
2021-04-26 13:00:46 +0200

    added imgs for git, first push of webvtt code and equalizer

--- public/js/index.js
+++ public/js/index.js
@@ -1,84 +1,82 @@
-//
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   const selector = '#player';
-//
-//   // Setup the player
-//   const player = new Plyr(selector, {
-//     debug: true,
-//     title: 'Westworld',
-//     iconUrl: 'https://cdn.plyr.io/3.6.7/plyr.svg',
-//     keyboard: {
-//       global: true,
-//     },
-//     tooltips: {
-//       controls: true,
-//     },
-//     captions: {
-//       active: true,
-//     },
-//     previewThumbnails: {
-//       enabled: true,
-//       src: ['https://cdn.plyr.io/static/demo/thumbs/100p.vtt', 'https://cdn.plyr.io/static/demo/thumbs/240p.vtt'],
-//     }
-//   });
-//
-//   // Expose for tinkering in the console
-//   window.player = player;
-//
-//   // const file = document.getElementById("player");
-//   //
-//   // const context = new AudioContext();
-//   // const src = context.createMediaElementSource(file.src);
-//   // const analyser = context.createAnalyser();
-//   //
-//   // const canvas = document.getElementById("canvas");
-//   // canvas.width = window.innerWidth;
-//   // canvas.height = window.innerHeight;
-//   // const ctx = canvas.getContext("2d");
-//   //
-//   // src.connect(analyser);
-//   // analyser.connect(context.destination);
-//   //
-//   // analyser.fftSize = 256;
-//   //
-//   // const bufferLength = analyser.frequencyBinCount;
-//   // console.log(bufferLength);
-//   //
-//   // const dataArray = new Uint8Array(bufferLength);
-//   //
-//   // const WIDTH = canvas.width;
-//   // const HEIGHT = canvas.height;
-//   //
-//   // const barWidth = (WIDTH / bufferLength) * 2.5;
-//   // let barHeight;
-//   // let x = 0;
-//   //
-//   // function renderFrame() {
-//   //   requestAnimationFrame(renderFrame);
-//   //
-//   //   x = 0;
-//   //
-//   //   analyser.getByteFrequencyData(dataArray);
-//   //
-//   //   ctx.fillStyle = "#000";
-//   //   ctx.fillRect(0, 0, WIDTH, HEIGHT);
-//   //
-//   //   for (const i = 0; i < bufferLength; i++) {
-//   //     barHeight = dataArray[i];
-//   //
-//   //     const r = barHeight + (25 * (i/bufferLength));
-//   //     const g = 250 * (i/bufferLength);
-//   //     const b = 50;
-//   //
-//   //     ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-//   //     ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
-//   //
-//   //     x += barWidth + 1;
-//   //   }
-//   //
-//   // }
-//   //
-//   // renderFrame();
-//
-// })
+document.addEventListener('DOMContentLoaded', () => {
+  let ind = 0;
+  const video = document.getElementById("player")
+
+  video.addEventListener('click', () => {
+
+    if(ind !== 0) {
+      console.log('hello')
+    } else {
+
+
+      const context = new (window.AudioContext || window.webkitAudioContext)()
+      const src = context.createMediaElementSource(video)
+      const analyser = context.createAnalyser()
+
+      // Set canvas width
+      const canvas = document.getElementById("canvas")
+      canvas.width = 600
+      canvas.height = 300
+
+      // Set context object
+      const ctx = canvas.getContext("2d")
+
+      // Connect the analyser to the video
+      src.connect(analyser)
+      analyser.connect(context.destination)
+
+      // Set buffer length
+      analyser.fftSize = 256
+      const bufferLength = analyser.frequencyBinCount
+
+      // Create an array of the buffers
+      const dataArray = new Uint8Array(bufferLength)
+
+      // Get canvas width
+      const WIDTH = canvas.width
+      const HEIGHT = canvas.height
+
+      // Set the width of a single bar
+      const barWidth = (WIDTH / bufferLength) * 1.5
+      let barHeight
+      let x = 0
+
+      function renderFrame() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // Callback to animate the next frame
+        requestAnimationFrame(renderFrame)
+
+        x = 0;
+
+        analyser.getByteFrequencyData(dataArray)
+
+        // Create each bar
+        for (let i = 0; i < bufferLength; i++) {
+
+
+          barHeight = dataArray[i]
+          // Make it a lil shorter
+          barHeight = barHeight < 20 ? barHeight : barHeight - 20
+
+          // ctx.globalAlpha = 1;
+
+          ctx.fillStyle = 'rgb(255,255,255)'
+          ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight)
+
+          x += barWidth + 15
+        }
+
+      }
+
+      renderFrame()
+
+      // Don't replace the canvas
+      ind++
+    }
+
+
+  }) // checks if element is playing right now
+
+
+
+})
